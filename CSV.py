c2e1a008f00b180746075d54d54ed604be6a83c9
xzibiputra <indrasani65@gmail.com>
2021-01-17 20:22:21 +0700

    datatablecsv

---
+++ CSV.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv("https://academy.dqlab.id/dataset/penduduk_gender_head.csv")
+table.head()
+x_label = table ['NAMA KELURAHAN']
+plt.bar(x=np.arange(len(x_label)),height=table['LAKI-LAKI WNI'])
+plt.xticks(np.arange(len(x_label)), table['NAMA KELURAHAN'], rotation = 90)
+plt.xlabel('Kelurahan di jakarta pusat')
+plt.ylabel('Jumlah laki laki')
+plt.title('Pesebaran Jumlah Penduduk Laki Laki di Jakarta Pusat')
+plt.show()
